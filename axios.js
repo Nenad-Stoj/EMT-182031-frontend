30f9b881ffb6528b35ce5e5430375547982535fb
Nenad-Stoj <104387319+Nenad-Stoj@users.noreply.github.com>
2023-04-30 20:55:05 +0200

    Delete axios.js

--- axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'https://emt-182031-backend.herokuapp.com/api',
-    headers: {"Access-Control-Allow-Origin": "*"}
-})
-
-export default instance;
